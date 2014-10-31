9a04c892b67b0b37aed37bc79c4315ead185c859
Homeski <homeski@cox.net>
2014-10-31 12:40:35 -0700

    Added some HTML for the advisor panel

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,8 +3,8 @@
 	<h1>Welcome!</h1>
 	<p>Please complete the following steps below to schedule an appointment with a CS adviser.</p>
 </div>
-<div class="panel panel-default">
-	<div class="panel-heading"><h3><strong>1. Category</strong></h3></div>
+<div class="panel panel-primary">
+	<div class="panel-heading"><h3><strong>1. Select a category</strong></h3></div>
 	<div class="panel-body">
 		<p>What's the reason for your appointment?</p>
 		{{ Form::open(array('url' => 'foo/bar')) }}			
@@ -12,6 +12,12 @@
 			{{ Form::select('category', $category, null, array('class' => 'form-control')); }}
 
 		{{ Form::close() }}
+	</div>
+</div>
+<div class="panel panel-primary">
+	<div class="panel-heading"><h3><strong>2. Select an advisor</strong></h3></div>
+	<div class="panel-body">
+		<p>Choose an advisor from the list below</p>
 	</div>
 </div>
 @stop
